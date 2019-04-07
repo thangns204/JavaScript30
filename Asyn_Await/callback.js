1882605f6c62f03ec1e24ade9283eb368ebf9a13
Ngo Thang <thangns@Ngos-MacBook-Pro.local>
2019-04-07 22:37:30 +0300

    Add Callback hell

--- Asyn_Await/callback.js
+++ Asyn_Await/callback.js
@@ -12,7 +12,41 @@ let add = (a, b, cb) => {
   }, 1000);
 }
 
-add(3, 4, (err, result) => {
+let multiply = (a, b, cb) => {
+  setTimeout(() => {
+    if (typeof a != 'number' || typeof b != 'number') {
+      return cb(new Error('Not a Number'));
+    }
+    cb(undefined, a * b);
+  }, 1000);
+}
+
+let divide = (a, b, cb) => {
+  setTimeout(() => {
+    if (typeof a != 'number' || typeof b != 'number') {
+      return cb(new Error('Not a Number'));
+    }
+    if (b == 0) return cb(new Error('div by 0'))
+    cb(undefined, a / b);
+  }, 1000);
+}
+
+
+let DT = (a, b, h, cb) => {
+  add(a, b, (err, result) =>{
+    if (err) return cb(err);
+    multiply(result, h, (err, result) => {
+      if (err) return cb(err);
+      divide(result, 2, (err, result) => {
+        if (err) return cb(err);
+        cb(undefined, result);
+      })
+    })
+  })
+}
+
+
+DT(2, 3, 4, (err, result) => {
   if (err) console.log(err); else {
     console.log(result);
   }
